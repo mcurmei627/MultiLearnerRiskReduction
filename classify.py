f4cd80dd13a0858b522bf09ac8541b4bac5628f8
Daniel Jiang <danji@bicycle.cs.washington.edu>
2023-04-13 16:48:47 -0700

    Cleanup classification code a litlte

--- classify.py
+++ classify.py
@@ -7,7 +7,7 @@ from sklearn.tree import DecisionTreeClassifier as DTC
 
 X, y = load_digits(return_X_y=True)
 
-class Dumb(object):
+class Random(object):
     def predict(self, X):
         return np.random.choice(10, size=X.shape[0])
 
@@ -19,12 +19,12 @@ class Constant(object):
         return np.ones(X.shape[0]) * self.val
 
 
-models = [Dumb() for _ in range(5)]
+models = [Random() for _ in range(5)]
 #models = [Constant(j) for j in range(5)]
 
 def fit(X_trn, y_trn):
     if X_trn.shape[0] == 0:
-        return Dumb()
+        return Random()
     if X_trn.shape[0] == 1:
         return Constant(y_trn[0])
     X_trn, X_val, y_trn, y_val = train_test_split(X_trn, y_trn, test_size=0.2)
@@ -53,24 +53,28 @@ def assign(idxs, models, null=False):
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
+    parser.add_argument('-k', type=int, default=30, help='number of customers sampled at each round')
+    parser.add_argument('-T', type=int, default=40, help='number of total rounds')
+    parser.add_argument('-t', type=int, default=0, help='number of monopoly rounds')
     args = parser.parse_args()
-    data = np.zeros((len(models), len(X)))
 
-    for _ in range(40):
-        idxs = np.random.choice(X.shape[0], size=30, replace=False)
+    data = np.zeros((len(models), len(X)))  # 1 if learner i has the data of customer j, 0 otherwise
+
+    for _ in range(args.T):
+        idxs = np.random.choice(X.shape[0], size=args.k, replace=False)
         idxs.sort()
         assignments = assign(idxs, models)
 
-        #if _ < 20:
-            #assignments = np.zeros(idxs.size)
+        if _ < args.t:
+            assignments = np.zeros(idxs.size)
 
         for i in range(len(models)):
             data[i][idxs[np.where(assignments == i)[0]]] = 1
             model_idxs = np.where(data[i] == 1)[0]
             models[i] = fit(X[model_idxs], y[model_idxs])
 
-
     assignments = assign(np.arange(X.shape[0]), models, null=True)
+    print('*' * 20)
     for i in range(-1, len(models)):
         counts = np.zeros(10)
         for label in y[np.where(assignments == i)]:
@@ -80,3 +84,5 @@ if __name__ == '__main__':
         else:
             print(str(models[i]) + ' correct')
         print(counts)
+        print()
+    print('*' * 20)
