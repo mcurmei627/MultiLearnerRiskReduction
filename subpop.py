98ef11e7e3f1fdd130a354cc2cb8e22a14441144
Mihaela <curmeimihaela@gmail.com>
2024-05-03 07:18:40 -0700

    Revert "Revert "Updating README""
    This reverts commit c0a2bd399f947e4e2313402229dc4db4e2fe6e73.

--- subpop.py
+++ subpop.py
@@ -3,6 +3,8 @@ import pandas as pd
 import folktables
 from sklearn.linear_model import LinearRegression
 
+import folktables
+
 
 class SubPop():
     def __init__(self, phi, beta, alphas, cov=None, sigmasq=1):
